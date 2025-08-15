977b2133a2f0839cee4e0842e5689613c0c82005
decimozs <marlonadiguemartin548@gmail.com>
2025-08-15 11:46:45 +0800

    fix: add suspense boundary for reset password form

--- components/auth/reset-password.tsx
+++ components/auth/reset-password.tsx
@@ -9,6 +9,7 @@ import { Button } from "../ui/button";
 import { Loader2 } from "lucide-react";
 import { useAuth } from "@/mutations/use-auth";
 import { redirect, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 const resetPasswordSchema = z
   .object({
@@ -56,72 +57,74 @@ export default function ResetPasswordForm() {
   });
 
   return (
-    <div className="p-8 h-screen w-full flex items-center justify-center">
-      <form
-        className="flex flex-col gap-4 w-full max-w-md"
-        onSubmit={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-          form.handleSubmit();
-        }}
-      >
-        <div>
-          <h1 className="text-2xl font-semibold">Kindred 🌟</h1>
-          <p>Reset your Password</p>
-        </div>
+    <Suspense>
+      <div className="p-8 h-screen w-full flex items-center justify-center">
+        <form
+          className="flex flex-col gap-4 w-full max-w-md"
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            form.handleSubmit();
+          }}
+        >
+          <div>
+            <h1 className="text-2xl font-semibold">Kindred 🌟</h1>
+            <p>Reset your Password</p>
+          </div>
 
-        <div className="*:not-first:mt-2">
-          <Label className="text-lg">New password</Label>
-          <form.Field name="newPassword">
-            {(field) => (
-              <>
-                <Input
-                  type="password"
-                  placeholder="Enter your new password"
-                  value={field.state.value}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                  field={field}
-                />
-                <FieldError field={field} />
-              </>
+          <div className="*:not-first:mt-2">
+            <Label className="text-lg">New password</Label>
+            <form.Field name="newPassword">
+              {(field) => (
+                <>
+                  <Input
+                    type="password"
+                    placeholder="Enter your new password"
+                    value={field.state.value}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                    field={field}
+                  />
+                  <FieldError field={field} />
+                </>
+              )}
+            </form.Field>
+          </div>
+
+          <div className="*:not-first:mt-2">
+            <Label className="text-lg">Confirm Password</Label>
+            <form.Field name="confirmPassword">
+              {(field) => (
+                <>
+                  <Input
+                    type="password"
+                    placeholder="Please confirm your password"
+                    value={field.state.value}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                    field={field}
+                  />
+                  <FieldError field={field} />
+                </>
+              )}
+            </form.Field>
+          </div>
+
+          <Button type="submit" className="w-full mt-4" disabled={isPending}>
+            {isPending ? (
+              <Loader2 className="ml-2 animate-spin" size={16} />
+            ) : (
+              "Reset Password"
             )}
-          </form.Field>
-        </div>
+          </Button>
 
-        <div className="*:not-first:mt-2">
-          <Label className="text-lg">Confirm Password</Label>
-          <form.Field name="confirmPassword">
-            {(field) => (
-              <>
-                <Input
-                  type="password"
-                  placeholder="Please confirm your password"
-                  value={field.state.value}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                  field={field}
-                />
-                <FieldError field={field} />
-              </>
-            )}
-          </form.Field>
-        </div>
-
-        <Button type="submit" className="w-full mt-4" disabled={isPending}>
-          {isPending ? (
-            <Loader2 className="ml-2 animate-spin" size={16} />
-          ) : (
-            "Reset Password"
-          )}
-        </Button>
-
-        <div className="text-center text-sm">
-          Already remember your password?{" "}
-          <a href="/sign-in" className="underline hover:no-underline">
-            Back to sign in
-          </a>
-          .
-        </div>
-      </form>
-    </div>
+          <div className="text-center text-sm">
+            Already remember your password?{" "}
+            <a href="/sign-in" className="underline hover:no-underline">
+              Back to sign in
+            </a>
+            .
+          </div>
+        </form>
+      </div>
+    </Suspense>
   );
 }
