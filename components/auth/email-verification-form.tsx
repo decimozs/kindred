3907f63c8a963b0310817d9ff22e7095623d860d
decimozs <marlonadiguemartin548@gmail.com>
2025-08-15 12:08:38 +0800

    feat(email-verification-form.tsx): add loading state for sending email verification

--- components/auth/email-verification-form.tsx
+++ components/auth/email-verification-form.tsx
@@ -8,6 +8,7 @@ import { formOptions, useForm } from "@tanstack/react-form";
 import { Button } from "../ui/button";
 import { Loader2 } from "lucide-react";
 import { useAuth } from "@/mutations/use-auth";
+import { useState } from "react";
 
 const emailVerificationSchema = z.object({
   email: z.email("Invalid email address"),
@@ -30,6 +31,7 @@ const formOpts = formOptions({
 
 export default function EmailVerificationForm() {
   const { requestPasswordReset } = useAuth();
+  const [isEmailSent, setEmailSent] = useState<boolean>(false);
   const isPending = requestPasswordReset.isPending;
 
   const form = useForm({
@@ -40,8 +42,13 @@ export default function EmailVerificationForm() {
         redirectTo: "http://localhost:3000/reset-password",
       });
       formApi.reset();
+      setEmailSent(true);
     },
   });
+
+  const handleResentVerification = () => {
+    setEmailSent(false);
+  };
 
   return (
     <div className="p-8 h-screen w-full flex items-center justify-center">
@@ -58,42 +65,61 @@ export default function EmailVerificationForm() {
           <p>Welcome back to Kindred!</p>
         </div>
 
-        <div className="*:not-first:mt-2">
-          <Label className="text-lg">Email</Label>
-          <form.Field name="email">
-            {(field) => (
-              <>
-                <Input
-                  type="email"
-                  placeholder="Enter your email"
-                  value={field.state.value}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                  field={field}
-                />
-                <FieldError field={field} />
-              </>
-            )}
-          </form.Field>
-        </div>
+        {isEmailSent ? (
+          <div className="rounded-md p-4 text-center border-[1px] border-dashed border-accent bg-accent/10">
+            <p className="text-sm">
+              A verification email has been sent to your email address. Please
+              check your inbox and follow the instructions to verify your
+              account. If you don&apos;t see the email, please check your spam
+              folder.
+            </p>
+          </div>
+        ) : (
+          <>
+            <div className="*:not-first:mt-2">
+              <Label className="text-lg">Email</Label>
+              <form.Field name="email">
+                {(field) => (
+                  <>
+                    <Input
+                      type="email"
+                      placeholder="Enter your email"
+                      value={field.state.value}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                      field={field}
+                    />
+                    <FieldError field={field} />
+                  </>
+                )}
+              </form.Field>
+            </div>
 
-        <Button type="submit" className="w-full mt-4" disabled={isPending}>
-          {isPending ? (
-            <Loader2 className="ml-2 animate-spin" size={16} />
-          ) : (
-            "Send Verification Email"
-          )}
-        </Button>
+            <Button type="submit" className="w-full mt-4" disabled={isPending}>
+              {isPending ? (
+                <Loader2 className="ml-2 animate-spin" size={16} />
+              ) : (
+                "Send Verification Email"
+              )}
+            </Button>
+          </>
+        )}
 
-        <div className="text-center text-sm">
-          Please verify your email to continue. Didn’t get the email?{" "}
-          <a
-            href="/resend-verification"
-            className="underline hover:no-underline"
-          >
-            Resend verification
-          </a>
-          .
-        </div>
+        {isEmailSent ? (
+          <div className="text-center text-sm">
+            Didn&apos;t get the email?{" "}
+            <a
+              className="underline hover:no-underline cursor-pointer"
+              onClick={handleResentVerification}
+            >
+              Resend verification
+            </a>
+            .
+          </div>
+        ) : (
+          <div className="text-center text-sm">
+            Please verify your email to continue.
+          </div>
+        )}
       </form>
     </div>
   );
